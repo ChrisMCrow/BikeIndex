fa59f76b0e00437bd63b28aef8ebce3ac6ca4d69
Chris Crow and Connor McCarthy <connor@>
2018-10-17 11:29:28 -0700

    update js files

--- src/bike.js
+++ src/bike.js
@@ -1,29 +1,29 @@
+const Promise = require('es6-promise').Promise;
+
 export class Bike{
-    constructor(location, color, manufacturer, year, picture){
-        this.location = location;
-        this.color = color;
-        this.manufacturer = manufacturer;
-        this.year = year;
-        this.picture = picture;
-    }
+  constructor(location, color, manufacturer, year, picture){
+    this.location = location;
+    this.color = color;
+    this.manufacturer = manufacturer;
+    this.year = year;
+    this.picture = picture;
+  }
 }
 
 export class Search{
-    getBikeByLocation(location, manufacturer){
-        return new Promise(function(resolve, reject){
-            let request = new XMLHttpRequest();
-            let url = `bikeindex.org/api/v3/search?API_KEY=${process.env.API_KEY}&page=1&per_page=25manufacturer=${manufacturer}&&location=${location}`;
-            request.onLoad = function(){
-                if(this.status === 200){
-                    resolve(request.response);
-                }else{
-                    reject(Error(request.statusText));
-                }
-            }
-            request.open("GET", url, true);
-            request.send();
-        });
-
-        getBikeByColor()
-    }
+  getBike(location, distance = 10, manufacturer) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://bikeindex.org/api/v3/search?API_KEY=${process.env.API_KEY}&page=1&per_page=1&location=${location}&distance=${distance}&stolenness=proximity&manufacturer=${manufacturer}`;
+      request.onLoad = function(){
+        if(this.status === 200){
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
--- src/main.js
+++ src/main.js
@@ -2,10 +2,28 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { Bike } from './bike.js';
+import {  Search } from './bike.js';
 
 
 $(document).ready(function() {
-    
+  $("#form").submit(function(event){
+    event.preventDefault();
 
+    let location = $("#location").val();
+    let distance = $("#distance").val();
+    let manufacturer = $("#manufacturer").val();
+
+    let newSearch = new Search();
+    let promise = newSearch.getBike(location, distance, manufacturer);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      console.log(response);
+      $('.showLocation').append(`Location: ${body.bikes}`);
+      $('.showManufacturer').text(`Manufacturer: ${body.bikes[0].manufacturer_name}`);
+
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
 });
