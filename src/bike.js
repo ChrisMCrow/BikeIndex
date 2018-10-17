75e5d3bcc4945a3963240ff6a33c0c3882c1cbaf
Chris Crow and Connor McCarthy <connor@>
2018-10-17 13:12:30 -0700

    return value from API call

--- src/bike.js
+++ src/bike.js
@@ -1,22 +1,22 @@
-const Promise = require('es6-promise').Promise;
-
-export class Bike{
-  constructor(location, color, manufacturer, year, picture){
-    this.location = location;
-    this.color = color;
-    this.manufacturer = manufacturer;
-    this.year = year;
-    this.picture = picture;
-  }
-}
+// export class Bike{
+//   constructor(location, color, manufacturer, year, picture){
+//     this.location = location;
+//     this.color = color;
+//     this.manufacturer = manufacturer;
+//     this.year = year;
+//     this.picture = picture;
+//   }
+// }
 
 export class Search{
-  getBike(location, distance = 10, manufacturer) {
+  getBike(location, distance, manufacturer) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `http://bikeindex.org/api/v3/search?API_KEY=${process.env.API_KEY}&page=1&per_page=1&location=${location}&distance=${distance}&stolenness=proximity&manufacturer=${manufacturer}`;
-      request.onLoad = function(){
+      request.onload = function(){
+        console.log("LOADED");
         if(this.status === 200){
+          console.log(request.response);
           resolve(request.response);
         } else {
           reject(Error(request.statusText));
--- src/main.js
+++ src/main.js
@@ -10,17 +10,16 @@ $(document).ready(function() {
     event.preventDefault();
 
     let location = $("#location").val();
-    let distance = $("#distance").val();
+    let distance = 10;
     let manufacturer = $("#manufacturer").val();
 
     let newSearch = new Search();
     let promise = newSearch.getBike(location, distance, manufacturer);
-
+    console.log(promise);
     promise.then(function(response) {
       let body = JSON.parse(response);
-      console.log(response);
-      $('.showLocation').append(`Location: ${body.bikes}`);
-      $('.showManufacturer').text(`Manufacturer: ${body.bikes[0].manufacturer_name}`);
+      $('.showLocation').append(`Location: ${body.bikes[0].stolen_location}`);
+      $('.showManufacturer').show(`Manufacturer: ${body.bikes[0].manufacturer_name}`);
 
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
